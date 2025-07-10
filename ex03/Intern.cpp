ed99ebf71ae210c6a3dd975f2f60c9c756fba429
Matteo Camilli <matteocamilli@Air-di-Matteo-3.Home>
2025-07-10 11:32:43 +0200

    finito, tolti container

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -4,21 +4,7 @@
 #include "RobotomyRequestForm.hpp"
 #include "PresidentialPardonForm.hpp"
 
-#include <map>
 #include <iostream>
-
-int Intern::resolveForm(const std::string& formName)
-{
-    static std::map<std::string,int> formMap;
-    if (formMap.empty()) {
-        formMap.insert(std::make_pair("shrubbery creation", FORM_ID_SHRUBBERY));
-        formMap.insert(std::make_pair("robotomy request",    FORM_ID_ROBOTOMY));
-        formMap.insert(std::make_pair("presidential pardon", FORM_ID_PARDON));
-    }
-
-    std::map<std::string,int>::const_iterator it = formMap.find(formName);
-    return (it != formMap.end()) ? it->second : FORM_ID_UNKNOWN;
-}
 
 Intern::Intern()
 {
@@ -39,15 +25,20 @@ Intern& Intern::operator=(const Intern&)
 AForm* Intern::makeForm(const std::string& formName, const std::string& target) const
 {
     AForm* form = NULL;
-    switch ( resolveForm(formName) )
+	if (formName != "shrubbery creation" && formName != "robotomy request" && formName != "presidential pardon")
+	{
+		std::cerr << "Intern error: \"" << formName << "\" is not a valid form name" << std::endl;
+		return (NULL);
+	}
+    switch ( formName.c_str()[0] )
     {
-      case FORM_ID_SHRUBBERY:
+      case 's':
         form = new ShrubberyCreationForm(target);
         break;
-      case FORM_ID_ROBOTOMY:
+      case 'r':
         form = new RobotomyRequestForm(target);
         break;
-      case FORM_ID_PARDON:
+      case 'p':
         form = new PresidentialPardonForm(target);
         break;
       default:
